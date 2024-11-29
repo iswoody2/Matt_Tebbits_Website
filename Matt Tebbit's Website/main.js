0dcb59b7771b7546a7428a9d232efa6785396a90
IanHunter_LawCode <153140620+iswoody2@users.noreply.github.com>
2024-11-29 15:16:55 +0000

    Delete Matt Tebbit's Website directory

--- Matt Tebbit's Website/main.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector(".menu-icon");
-const mobileMenu = document.querySelector(".mobile-menu");
-
-menuIcon.addEventListener("click", () => {
-    mobileMenu.classList.toggle("active");
-} )
